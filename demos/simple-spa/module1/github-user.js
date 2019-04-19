26032fc05f15dc6bf3074ce82514f593fc6c56a0
Vedran Bilopavlović <vbilopav@gmail.com>
2019-04-19 11:27:45 +0200

    format html

--- demos/simple-spa/module1/github-user.js
+++ demos/simple-spa/module1/github-user.js
@@ -10,7 +10,7 @@ define([], () => class {
             <h1>Github user data</h1>
             <p> 
                 <label for="username">Enter valid github username: </label> 
-                <input label type="text" id="username" autofocus spellcheck="false" onkeypress="e => e.keyCode === 13 && this.model.btn.click()">
+                <input type="text" id="username" autofocus spellcheck="false" onkeypress="e => e.keyCode === 13 && this.model.btn.click()">
                 <br />
                 <button name="btn" onclick="showClick">Show github user data</button>
             </p>
