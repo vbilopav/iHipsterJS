d811ac6ee2f7833aa70fa9e202bfbe443077b1ee
Vedran Bilopavlović <vbilopav@gmail.com>
2019-06-15 17:28:04 +0200

    add prism to demos index

--- demos/shared/themes-switcher.js
+++ demos/shared/themes-switcher.js
@@ -4,18 +4,26 @@
         key = 'ihjs-demos-theme',
         current = localStorage.getItem(key),
         opposite = function(t) {
-            return t === 'dark' ? 'light' : 'dark' 
+            return t === 'dark' ? 'light' : 'dark';
         };
+
     if (!current) {
         current = "dark";
     };
     
-    var css = document.createElement("LINK");
-    css.async = true;
-    css.href = "/demos/shared/css/water/" + current + ".css";
-    css.rel = "stylesheet";
-    document.head.appendChild(css);
-    css.onload = function() {
+    var css1 = document.createElement("LINK");
+    css1.async = true;
+    css1.href = "/demos/shared/css/water/" + current + ".css";
+    css1.rel = "stylesheet";
+    document.head.appendChild(css1);
+
+    var css2 = document.createElement("LINK");
+    css2.async = true;
+    css2.href = "/demos/shared/prism/" + current + ".css";
+    css2.rel = "stylesheet";
+    document.head.appendChild(css2);
+
+    css1.onload = function() {
 
         var wrap = document.createElement("DIV");
         wrap.style.position = "fixed";
@@ -34,11 +42,14 @@
         document.body.appendChild(wrap);
     
         btn.addEventListener("click", function() {
-            css.href = css.href.replace(/dark|light/, function(replaced) { 
+            css1.href = css1.href.replace(/dark|light/, function(replaced) { 
                 var theme = opposite(replaced);
                 btn.innerHTML = "switch to " + opposite(theme) + " theme";
                 localStorage.setItem(key, theme);
                 return theme; 
+            });
+            css2.href = css2.href.replace(/dark|light/, function(replaced) { 
+                return opposite(replaced);
             });
         });
 
